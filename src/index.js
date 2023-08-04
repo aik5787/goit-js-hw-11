290467022890faf54a480b9f54f153f455a9ee88
Aleksandr <aik5787@gmail.com>
2023-08-04 12:17:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import axios from "axios";
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
 // const apiKey = "live_y93JF5d0g1wRGcfrQtzrygacDWrb0A3StZkiB2CW48e50u7WneMRxFx9q5nyIgQt";
 // axios.defaults.headers.common["x-api-key"] = apiKey;
